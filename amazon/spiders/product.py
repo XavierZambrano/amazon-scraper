a8de84ca54d86765dfcb585342c9951c949d84ee
XavierZambrano <xavierzamco@gmail.com>
2024-07-20 14:37:47 -0600

    feat: for the price extract also the currency

--- amazon/spiders/product.py
+++ amazon/spiders/product.py
@@ -12,9 +12,12 @@ class ProductSpider(scrapy.Spider):
 
     def parse(self, response):
         title = response.xpath('//h1[@id="title"]//text()[normalize-space()]').get()
-        price_raw = response.xpath('//div[@class="cardRoot bucket"]/@data-price-totals').get()
+        price_raw = response.xpath('//div[@class="cardRoot bucket"]/@data-components').get()
         price_raw = json.loads(price_raw)
-        price = price_raw['1']
+        price = {
+            'currency': price_raw['1']['price']['currencyCode'],
+            'value': float(price_raw['1']['price']['wholeValue'] + '.' + price_raw['1']['price']['fractionalValue'])
+        }
 
         yield {
             'asin': response.xpath('//input[@name="items[0.base][asin]"]/@value').get(),
