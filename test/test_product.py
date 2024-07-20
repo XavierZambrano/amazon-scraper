261ed3e9e0bb199ed7908fa1586ddea63bd8ff9c
XavierZambrano <xavierzamco@gmail.com>
2024-07-20 14:04:16 -0600

    test: add tests

---
+++ test/test_product.py
@@ -0,0 +1,59 @@
+import os
+import json
+from betamax import Betamax
+from betamax.fixtures.unittest import BetamaxTestCase
+from scrapy.crawler import CrawlerProcess
+from scrapy.http import HtmlResponse
+
+from amazon.spiders.product import ProductSpider
+
+
+with Betamax.configure() as config:
+    cassette_library_dir = 'test/fixtures/cassettes'
+    os.makedirs(cassette_library_dir, exist_ok=True)
+    config.cassette_library_dir = cassette_library_dir
+
+
+class TestProductSpider(BetamaxTestCase):
+    def setUp(self):
+        super().setUp()
+
+        process = CrawlerProcess(install_root_handler=False)
+        process.crawl(ProductSpider)
+        self.spider = list(process.crawlers)[0].spider
+
+    def test_parse_asin(self):
+        mock_response = self.get_mock_response('https://www.amazon.com/War-Peace-Penguin-Clothbound-Classics/dp/0241265541/')
+        expected_result = '0241265541'
+
+        generator = self.spider.parse(mock_response)
+        result = next(generator)
+
+        self.assertEqual(result['asin'], expected_result)
+
+    def test_parse_title(self):
+        mock_response = self.get_mock_response('https://www.amazon.com/War-Peace-Penguin-Clothbound-Classics/dp/0241265541/')
+        expected_result = 'War and Peace (Penguin Clothbound Classics)'
+
+        generator = self.spider.parse(mock_response)
+        result = next(generator)
+
+        self.assertEqual(result['title'], expected_result)
+
+    def test_parse_price(self):
+        mock_response = self.get_mock_response('https://www.amazon.com/War-Peace-Penguin-Clothbound-Classics/dp/0241265541/')
+        expected_result = {
+            'currency': 'USD',
+            'value': 16.59
+        }
+
+        generator = self.spider.parse(mock_response)
+        result = next(generator)
+
+        self.assertEqual(result['price'], expected_result)
+
+    def get_mock_response(self, url):
+        response = self.session.get(url)
+        scrapy_response = HtmlResponse(body=response.content, url=url)
+
+        return scrapy_response
